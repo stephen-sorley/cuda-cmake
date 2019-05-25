fd3480e4dd4fe44d23d4e2898a4839bf66979a1f
Stephen Sorley <stephen.sorley@gmail.com>
2019-05-24 23:16:28 -0400

    InstallDeps.cmake: added missing license info

--- cmake/InstallDeps.cmake
+++ cmake/InstallDeps.cmake
@@ -23,9 +23,6 @@
 # stored in the ELF header is "libz.so.1". This function will install a file named "libz.so.1.2.11",
 # and a symlink that points to that file named "libz.so.1".
 #
-# This file was adapted from the mstdlib project (MIT licensed), found here:
-#   https://github.com/Monetra/mstdlib/blob/master/CMakeModules/InstallDepLibs.cmake
-#
 # Functions:
 # install_deps([lib dest] [runtime dest] [... lib files or import targets ...])
 #     Install the given list of lib dependencies to CMAKE_INSTALL_PREFIX, alongside the project.
@@ -48,6 +45,35 @@
 # INSTALL_DEPS_COPY_DLL
 #   If TRUE (the default), any DLL's installed with install_deps will also be copied to the build
 #   directory (specifically, to CMAKE_RUNTIME_OUTPUT_DIR). Set to FALSE to disable DLL copies.
+#
+# # # # # # # # # # # #
+# This file was originally adapted from the mstdlib project (also MIT licensed), found here:
+#   https://github.com/Monetra/mstdlib/blob/master/CMakeModules/InstallDepLibs.cmake
+#   (Monetra Technologies, LLC)
+#
+# # # # # # # # # # # #
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Stephen Sorley
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# # # # # # # # # # # #
 #
 cmake_minimum_required(VERSION 3.14)
 
